db1945b512855e267272b3a125991bf26a0cba0a
Adham <adhmwaled94@gmail.com>
2026-05-30 08:31:24 +0300

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cs
@@ -0,0 +1,27 @@
+public class Solution {
+    public bool IsAnagram(string s, string t) {
+        if(s.Length != t.Length )
+            return false;
+
+        Dictionary<char,int> freq = new Dictionary<char,int>();
+        foreach(char c in s){
+            if(freq.ContainsKey(c)){
+                freq[c]++;
+            }
+            else{
+                freq.Add(c, 1);
+            }
+        }
+        foreach(char c in t){
+            if(freq.ContainsKey(c)){
+                if(freq[c] > 0)
+                    freq[c]--;
+                else
+                    return false;
+            }
+            else
+                return false;
+        }
+        return true;
+    }
+}
